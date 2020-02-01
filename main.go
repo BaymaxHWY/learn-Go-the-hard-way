b26d307bfab833181ee1f642fcb9eecd6607fa5d
BaymaxHWY <854541526@qq.com>
2020-02-01 14:14:39 +0800

    L4 finish

--- main.go
+++ main.go
@@ -11,7 +11,30 @@ func MakeMap(fpt interface{}) {
 }
 
 //TODO:completes implMap function.
-var implMap func([]reflect.Value) []reflect.Value
+var implMap func([]reflect.Value) []reflect.Value = func(values []reflect.Value) []reflect.Value {
+	var output []reflect.Value
+	if len(values) <= 1 {
+		return nil
+	}
+	arg0 := values[0]
+	arg1 := values[1]
+	switch arg1.Kind() {
+	case reflect.Map:
+		keys := arg1.MapKeys()
+		for _, key := range keys {
+			v := arg1.MapIndex(key)
+			res := arg0.Call([]reflect.Value{v})
+			arg1.SetMapIndex(key, res[0])
+		}
+	case reflect.Slice:
+		for i := 0; i < arg1.Len(); i++ {
+			ret := arg0.Call([]reflect.Value{arg1.Index(i)})
+			arg1.Index(i).Set(ret[0])
+		}
+	}
+	output = append(output, arg1)
+	return output
+}
 
 func main() {
 
